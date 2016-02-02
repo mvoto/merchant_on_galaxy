2ae8650d0da8ea1e098e831f222224941550a40b
Mauricio Voto <mauriciovoto@gmail.com>
2016-02-02 04:20:17 -0200

    update specs

--- spec/lib/file_reader_spec.rb
+++ spec/lib/file_reader_spec.rb
@@ -1,77 +1,79 @@
 describe FileReader do
-  context 'given a valid input file' do
-    let(:valid_input_file) { './input_sample.txt' }
-    let(:intergalactics) do
-      [
-        "glob is I",
-        "prok is V",
-        "pish is X",
-        "tegj is L",
-      ]
+  describe '.perform' do
+    context 'given a valid input file' do
+      let(:valid_input_file) { './input_sample.txt' }
+      let(:intergalactics) do
+        [
+          "glob is I",
+          "prok is V",
+          "pish is X",
+          "tegj is L",
+        ]
+      end
+      let(:credits) do
+        [
+          "glob glob Silver is 34 Credits",
+          "glob prok Gold is 57800 Credits",
+          "pish pish Iron is 3910 Credits"
+        ]
+      end
+      let(:questions) do
+        [
+          "how much is pish tegj glob glob ?",
+          "how many Credits is glob prok Silver ?",
+          "how many Credits is glob prok Gold ?",
+          "how many Credits is glob prok Iron ?",
+          "how much wood could a wood chuck chuck if a wood chuck could chuck wood ?"
+        ]
+      end
+      let(:expected_content) do
+        { intergalactics: intergalactics, credits: credits, questions: questions }
+      end
+      subject { described_class.perform(valid_input_file) }
+
+      it 'defines intergalactics, credits and questions from file content' do
+        expect(subject).to eq(expected_content)
+      end
     end
-    let(:credits) do
-      [
-        "glob glob Silver is 34 Credits",
-        "glob prok Gold is 57800 Credits",
-        "pish pish Iron is 3910 Credits"
-      ]
+
+    context 'given an input file without intergalactics' do
+      let(:no_intergalactics_input) { './spec/support/no_intergalactics_input.txt' }
+
+      it 'raises Invalid Content Error' do
+        expect { described_class.perform(no_intergalactics_input) }.to raise_error(
+          InvalidContentError
+        )
+      end
     end
-    let(:questions) do
-      [
-        "how much is pish tegj glob glob ?",
-        "how many Credits is glob prok Silver ?",
-        "how many Credits is glob prok Gold ?",
-        "how many Credits is glob prok Iron?",
-        "how much wood could a wood chuck chuck if a wood chuck could chuck wood ?"
-      ]
+
+    context 'given an input file without credits' do
+      let(:no_credits_input) { './spec/support/no_credits_input.txt' }
+
+      it 'raises Invalid Content Error' do
+        expect { described_class.perform(no_credits_input) }.to raise_error(
+          InvalidContentError
+        )
+      end
     end
-    let(:expected_content) do
-      { intergalactics: intergalactics, credits: credits, questions: questions }
+
+    context 'given an input file without questions' do
+      let(:no_questions_input) { './spec/support/no_questions_input.txt' }
+
+      it 'raises Invalid Content Error' do
+        expect { described_class.perform(no_questions_input) }.to raise_error(
+          InvalidContentError
+        )
+      end
     end
-    subject { described_class.perform(valid_input_file) }
 
-    it 'defines intergalactics, credits and questions from file content' do
-      expect(subject).to eq(expected_content)
-    end
-  end
+    context 'given a non existing input file' do
+      let(:invalid_input_file) { './ghost_file.txt' }
 
-  context 'given an input file without intergalactics' do
-    let(:no_intergalactics_input) { './spec/support/no_intergalactics_input.txt' }
-
-    it 'raises Invalid Content Error' do
-      expect { described_class.perform(no_intergalactics_input) }.to raise_error(
-        InvalidContentError
-      )
-    end
-  end
-
-  context 'given an input file without credits' do
-    let(:no_credits_input) { './spec/support/no_credits_input.txt' }
-
-    it 'raises Invalid Content Error' do
-      expect { described_class.perform(no_credits_input) }.to raise_error(
-        InvalidContentError
-      )
-    end
-  end
-
-  context 'given an input file without questions' do
-    let(:no_questions_input) { './spec/support/no_questions_input.txt' }
-
-    it 'raises Invalid Content Error' do
-      expect { described_class.perform(no_questions_input) }.to raise_error(
-        InvalidContentError
-      )
-    end
-  end
-
-  context 'given a non existing input file' do
-    let(:invalid_input_file) { './ghost_file.txt' }
-
-    it 'raises Invalid Input Error' do
-      expect{ described_class.perform(invalid_input_file) }.to raise_error(
-        InvalidInputError
-      )
+      it 'raises Invalid Input Error' do
+        expect{ described_class.perform(invalid_input_file) }.to raise_error(
+          InvalidInputError
+        )
+      end
     end
   end
 end
--- spec/lib/translator_spec.rb
+++ spec/lib/translator_spec.rb
@@ -1,28 +1,58 @@
 describe Translator do
-  context 'given rules and questions' do
-    it 'defines intergalactic from sentence' do
+  describe '.new' do
+    context 'given intergalactics, credits and questions' do
+      let(:valid_args) do
+        {
+          intergalactics: ['glob is I', 'prok is V'],
+          credits: ['glob glob Silver is 8 Credits', 'glob prok Gold is 40 Credits'],
+          questions: ['How many Credits is glob prok Silver ?']
+        }
+      end
+      subject { described_class.new(valid_args) }
 
+      it 'defines intergalactics hash' do
+        expect(subject.intergalactic_hash).to eq({ glob: 'I', prok: 'V' })
+      end
+
+      it 'defines minerals hash' do
+        expect(subject.mineral_hash).to eq({ :Silver => 4, :Gold => 10 })
+      end
     end
 
-    it 'defines credits sentence' do
+    context 'given no intergalactics args' do
+      let(:no_rules_args) do
+        { intergalactics: [], credits: ['a', 'credit'], questions: ['this', 'question'] }
+      end
 
+      it 'raises Missing Rules Error' do
+        expect{ described_class.new(no_rules_args) }.to raise_error(MissingRulesError)
+      end
     end
-  end
 
-  context 'given no rules args' do
-    let(:no_rules_args) { { rules: [], questions: ['this', 'question'] } }
+    context 'given no credits args' do
+      let(:no_rules_args) do
+        { intergalactics: ['a', 'interg'], credits: [], questions: ['this', 'question'] }
+      end
 
-    it 'raises Missing Rules Error' do
-      expect{ described_class.new(no_rules_args) }.to raise_error(MissingRulesError)
+      it 'raises Missing Rules Error' do
+        expect{ described_class.new(no_rules_args) }.to raise_error(MissingRulesError)
+      end
     end
-  end
 
-  context 'given no questions args' do
-    let(:no_questions_args) { { rules: ['this', 'rule'], questions: [] } }
-    it 'raises Missing Questions Error' do
-      expect{ described_class.new(no_questions_args) }.to raise_error(
-        MissingQuestionsError
-      )
+    context 'given no questions args' do
+      let(:no_questions_args) do
+        {
+          intergalactics: ['glob is I'],
+          credits: ['glob glob Silver is 10 Credits'],
+          questions: []
+        }
+      end
+
+      it 'raises Missing Questions Error' do
+        expect{ described_class.new(no_questions_args) }.to raise_error(
+          MissingQuestionsError
+        )
+      end
     end
   end
 end
